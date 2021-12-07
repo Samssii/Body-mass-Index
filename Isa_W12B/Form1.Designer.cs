e36933fab9d6d5eeeb7a37e47ecd2bb2c71933c6
samsi <aadegtc@gmail.com>
2021-12-07 10:33:05 +0700

    Add project files.

---
+++ Isa_W12B/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace _Isa_W12B
+{
+    partial class FormBMI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.listBoxInfo = new System.Windows.Forms.ListBox();
+            this.labelEqual = new System.Windows.Forms.Label();
+            this.labelDash = new System.Windows.Forms.Label();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.textBoxWeight = new System.Windows.Forms.TextBox();
+            this.labelWeight = new System.Windows.Forms.Label();
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(128, 94);
+            this.buttonCalculate.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(72, 23);
+            this.buttonCalculate.TabIndex = 15;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // listBoxInfo
+            // 
+            this.listBoxInfo.FormattingEnabled = true;
+            this.listBoxInfo.Location = new System.Drawing.Point(180, 22);
+            this.listBoxInfo.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxInfo.Name = "listBoxInfo";
+            this.listBoxInfo.Size = new System.Drawing.Size(175, 56);
+            this.listBoxInfo.TabIndex = 14;
+            // 
+            // labelEqual
+            // 
+            this.labelEqual.AutoSize = true;
+            this.labelEqual.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEqual.Location = new System.Drawing.Point(157, 36);
+            this.labelEqual.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelEqual.Name = "labelEqual";
+            this.labelEqual.Size = new System.Drawing.Size(18, 20);
+            this.labelEqual.TabIndex = 13;
+            this.labelEqual.Text = "=";
+            // 
+            // labelDash
+            // 
+            this.labelDash.AutoSize = true;
+            this.labelDash.Location = new System.Drawing.Point(51, 36);
+            this.labelDash.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelDash.Name = "labelDash";
+            this.labelDash.Size = new System.Drawing.Size(103, 13);
+            this.labelDash.TabIndex = 12;
+            this.labelDash.Text = "________________";
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(66, 56);
+            this.textBoxHeight.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(76, 20);
+            this.textBoxHeight.TabIndex = 11;
+            // 
+            // textBoxWeight
+            // 
+            this.textBoxWeight.Location = new System.Drawing.Point(66, 19);
+            this.textBoxWeight.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxWeight.Name = "textBoxWeight";
+            this.textBoxWeight.Size = new System.Drawing.Size(76, 20);
+            this.textBoxWeight.TabIndex = 10;
+            // 
+            // labelWeight
+            // 
+            this.labelWeight.AutoSize = true;
+            this.labelWeight.Location = new System.Drawing.Point(9, 22);
+            this.labelWeight.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelWeight.Name = "labelWeight";
+            this.labelWeight.Size = new System.Drawing.Size(44, 13);
+            this.labelWeight.TabIndex = 9;
+            this.labelWeight.Text = "Weight:";
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Location = new System.Drawing.Point(9, 58);
+            this.labelHeight.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(41, 13);
+            this.labelHeight.TabIndex = 8;
+            this.labelHeight.Text = "Height:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 125);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.listBoxInfo);
+            this.Controls.Add(this.labelEqual);
+            this.Controls.Add(this.labelDash);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxWeight);
+            this.Controls.Add(this.labelWeight);
+            this.Controls.Add(this.labelHeight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.ListBox listBoxInfo;
+        private System.Windows.Forms.Label labelEqual;
+        private System.Windows.Forms.Label labelDash;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.TextBox textBoxWeight;
+        private System.Windows.Forms.Label labelWeight;
+        private System.Windows.Forms.Label labelHeight;
+    }
+}
+
---
+++ Isa_W12B/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _Isa_W12B
+{
+    public partial class FormBMI : Form
+    {
+        public FormBMI()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            listBoxInfo.Items.Clear();
+            double height = Double.Parse(textBoxHeight.Text);
+            double weight = Double.Parse(textBoxWeight.Text);
+            listBoxInfo.Items.Add(BodyMassIndex(weight, height));
+            listBoxInfo.HorizontalScrollbar = true;
+        }
+
+        private string BodyMassIndex(double weight, double height)
+        {
+            string result = "";
+            double BMI = Math.Round(weight / Math.Pow(height / 100, 2), 1);
+            if (BMI < 18.5)
+            {
+                result = "Your BMI = " + BMI + ", you are Underweight.";
+            }
+            else if (BMI >= 18.5 && BMI < 25)
+            {
+                result = "Your BMI = " + BMI + ", you are Normal.";
+            }
+            else if (BMI >= 25 && BMI < 30)
+            {
+                result = "Your BMI = " + BMI + ", you are Overweight.";
+            }
+            else if (BMI >= 30)
+            {
+                result = "Your BMI = " + BMI + ", you are Obese.";
+            }
+            return result;
+        }
+
+        private void labelWeight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxWeight_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxHeight_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelDash_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelEqual_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxInfo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelHeight_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
